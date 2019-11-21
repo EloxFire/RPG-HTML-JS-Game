fd0783bc9b1493a012b1d2bdc2a0dd6539816ceb
Enzo Avagliano <enzo.avagliano@ynov.com>
2019-11-21 16:06:07 +0100

    Adding Game and caracter creation

---
+++ assets/js/game.js
@@ -0,0 +1,70 @@
+/*////////////////////////////////////////////*/
+/* Program name : The Stolen Plans            */
+/* Program author : Enzo 'EloxFire Avagliano' */
+/* Program creation date : Nov-21-19          */
+/*                                            */
+/* Program version : 1.0                      */
+/*////////////////////////////////////////////*/
+
+
+// Player Variables
+let playerName;
+let playerGender;
+let playerMindset;
+
+// Game variables
+let gameDifficulty;
+let defaultHp = 20;
+let defaultSp = 20;
+let defaultExp = 0;
+
+// DOM
+let nameInput = document.getElementById('playerName');
+let genderInput = document.getElementById('playerGender');
+
+class Player{
+  constructor(name, gender, hp, sp, exp, grade){
+    this.name = name;
+    this.gender = gender;
+    this.hp = hp;
+    this.sp = sp;
+    this.exp = exp;
+    this.grade = grade;
+  }
+
+  getName(){
+    return this.name;
+  }
+  getGender(){
+    return this.gender;
+  }
+  getHp(){
+    return this.hp;
+  }
+  getSp(){
+    return this.sp;
+  }
+  getExp(){
+    return this.exp;
+  }
+  getGrade(){
+    return this.grade;
+  }
+}
+
+function setPlayerInfos(){
+  let nameInput = document.getElementById('playerName');
+  let genderInput = document.getElementById('playerGender');
+  playerName = nameInput.value;
+  playerGender = genderInput.value;
+
+  let player = new Player(playerName, playerGender, defaultHp, defaultSp, defaultExp, "Heros");
+
+  console.log("Player's name : " + player.getName());
+  console.log("Player's gender : " + player.getGender());
+  console.log("Player's HP : " + player.getHp());
+  console.log("Player's SP : " + player.getSp());
+  console.log("Player's experience : " + player.getExp());
+  console.log(' ');
+  console.log("Player infos were set successfully !");
+}
